6ba91bd1a8b725ec3dbfb3dbb5373f1aba448d42
vinson <dagger1245653@gmail.com>
2021-12-05 12:29:16 -0800

    add create post bar

--- reddit/src/Components/Create.js
+++ reddit/src/Components/Create.js
@@ -4,8 +4,8 @@ function Create() {
     return <div id="createBar">
         <img id="createImage" alt="subreddit" src="https://i.pinimg.com/474x/65/25/a0/6525a08f1df98a2e3a545fe2ace4be47.jpg" />
         <input id="createInput" type="text" placeholder="Create Post" />
-        <span className="material-icons"> insert_photo </span>
-        <span className="material-icons"> linkd </span>
+        <span className="material-icons createIcon" id="insertImage"> insert_photo </span>
+        <span className="material-icons createIcon" id="link"> link </span>
     </div>
 }
 
